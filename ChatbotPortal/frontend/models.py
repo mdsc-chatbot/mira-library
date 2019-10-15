e28cdba854d1dc4ebcef744c5457aa637eb87010
Jacy <jacygmark@gmail.com>
2019-10-14 18:05:56 -0600

    created reviews model

--- ChatbotPortal/frontend/models.py
+++ ChatbotPortal/frontend/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from enum import Enum
 
 # Create your models here.
+class Reviews(models.Model):
+    class approvalChoices(Enum):
+        Approved = 'APP'
+        Rejected = 'REJ'
+    
+    class types(Enum):
+        type1 = 1
+        type2 = 2
+
+    review_id = models.PositiveIntegerField(primary_key=True)
+    user_id = models.PositiveIntegerField()
+    review_types = types
+    default = types.type1
+    approved = approvalChoices
+    default = approvalChoices.Rejected
