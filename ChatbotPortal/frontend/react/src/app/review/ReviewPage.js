65c400b69dda5868166e565c12a850db068c8f4a
Jacy <jacygmark@gmail.com>
2019-10-14 18:45:36 -0600

    started reviewer UI

--- ChatbotPortal/frontend/react/src/app/review/ReviewPage.js
+++ ChatbotPortal/frontend/react/src/app/review/ReviewPage.js
@@ -2,8 +2,27 @@ import React from 'react';
 
 export default function ReviewPage() {
 	return (
-		<div>
-			This is an example review page!
-		</div>
+		<container>
+			<table class="ui celled table">
+				<thead>
+					<tr>
+					<th>URL</th>
+					<th>Type</th>
+					<th>Tags</th>
+					<th></th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+					<td>test1</td>
+					<td>Disability</td>
+					<td>ADHD, ADD</td>
+					<td>
+						<button class="positive ui button">Approve</button><button class="negative ui button">Reject</button> <label></label>
+					</td>
+					</tr>
+				</tbody>
+			</table>
+		</container>
 	);
 }
