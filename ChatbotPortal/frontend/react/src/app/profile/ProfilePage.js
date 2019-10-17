a91ee0185afeda8f9162b095e6e0de527f73be68
apu031 <touhidul.apu@gmail.com>
2019-10-17 15:42:15 -0600

    Initial Profile Page (Pre-editting feature)

--- ChatbotPortal/frontend/react/src/app/profile/ProfilePage.js
+++ ChatbotPortal/frontend/react/src/app/profile/ProfilePage.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import {SecurityContext} from '../security/SecurityContext';
+import Nav from "../authentication/Nav";
+import LoginForm from "../authentication/LoginForm";
 
 export default function ProfilePage() {
 	return (
 		<div>
-			This is an example profile page!
+			<SecurityContext.Consumer>
+                {(securityContext) => (
+                	<div>
+                        <h3>
+                            {securityContext.security.logged_in
+                                ? 	`Email: ${securityContext.security.email} '\n'
+                                	 First Name: ${securityContext.security.first_name} '\n'
+                                	 Last Name: ${securityContext.security.last_name} '\n'
+                                	 Affiliation: ${securityContext.security.affiliation} '\n'
+                                	 Active: ${securityContext.security.active} '\n'
+                                	 Staff: ${securityContext.security.staff} '\n'
+                                	 Admin: ${securityContext.security.admin}`
+                                : 'No Security Context'}
+                        </h3>
+                    </div>
+                )}
+            </SecurityContext.Consumer>
 		</div>
 	);
 }
