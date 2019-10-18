820adec3ce05f813c8c54b6ae01e568b4dd90b8c
tnguyen10 <tnguyen1@ualberta.ca>
2019-10-17 23:36:37 -0600

    backend: update requirement.txt, webscrape webpage title using beautiful soup

--- ChatbotPortal/resource/models.py
+++ ChatbotPortal/resource/models.py
@@ -1,6 +1,29 @@
 from django.db import models
 from django.core.validators import URLValidator, MaxValueValidator, MinValueValidator
 
+import urllib
+import urllib.request
+from bs4 import BeautifulSoup
+
+
+class ResourceManager(models.Manager):
+    def create(self, **obj_data):
+
+        # Get website actual title
+        url = obj_data['url']
+        r = urllib.request.Request(url, headers={
+                                   'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'})
+        html = urllib.request.urlopen(r).read().decode('utf8')
+
+        soup = BeautifulSoup(html, 'html.parser')
+        title = soup.find('title').string
+        if title:
+            obj_data['title'] = title
+        else:
+            obj_data['title'] = "Unknown title"
+
+        return super().create(**obj_data)
+
 
 class Resource(models.Model):
 
@@ -20,6 +43,8 @@ class Resource(models.Model):
     website_title = models.TextField(blank=True)
 
     score = models.DecimalField(max_digits=10, decimal_places=1)
+
+    objects = ResourceManager()
 
 
 class Tag(models.Model):
