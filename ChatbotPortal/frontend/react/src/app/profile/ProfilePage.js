bdbc8febbcd773fcee2ec5e8fa6bf4ad82337a2e
apu031 <touhidul.apu@gmail.com>
2019-10-20 10:44:24 -0600

    EditBegins

--- ChatbotPortal/frontend/react/src/app/profile/ProfilePage.js
+++ ChatbotPortal/frontend/react/src/app/profile/ProfilePage.js
@@ -1,28 +1,108 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {SecurityContext} from '../security/SecurityContext';
-import Nav from "../authentication/Nav";
-import LoginForm from "../authentication/LoginForm";
+import EditForm from "./EditForm";
 
-export default function ProfilePage() {
-	return (
-		<div>
+
+class ProfilePage extends Component {
+
+	state = {
+		token: '',
+		displayed_form: 'edit',
+		logged_in: localStorage.getItem('token') ? true : false,
+		id: '',
+		email: '',
+		first_name: '',
+		last_name: '',
+		is_edited: false,
+		url: ''
+	};
+
+	handle_edit = (e, data, setSecurity) => {
+		e.preventDefault();
+		fetch(`http://localhost:8000/4/update/`, {
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json',
+				'Accept': 'application/json'
+			},
+			body: JSON.stringify(data)
+		})
+			.then(res => res.json())
+			.then(json => {
+				setSecurity({
+					logged_in: true,
+					id: json.id,
+					email: json.email,
+					first_name: json.first_name,
+					last_name: json.last_name
+				});
+				this.setState({
+                    logged_in: true,
+                    id: json.id,
+                    email: json.email,
+                    first_name: json.first_name,
+                    last_name: json.last_name,
+                });
+			});
+	};
+
+	updateURL = pk => {
+		this.setState({
+			url: `http://localhost:8000/${pk}/update/`
+		})
+	};
+
+	is_edited = () => {
+		this.setState({
+			edited: true
+		});
+	};
+
+	render() {
+		return(
 			<SecurityContext.Consumer>
-                {(securityContext) => (
-                	<div>
-                        <h3>
-                            {securityContext.security.logged_in
-                                ? 	`Email: ${securityContext.security.email} '\n'
-                                	 First Name: ${securityContext.security.first_name} '\n'
-                                	 Last Name: ${securityContext.security.last_name} '\n'
-                                	 Affiliation: ${securityContext.security.affiliation} '\n'
-                                	 Active: ${securityContext.security.active} '\n'
-                                	 Staff: ${securityContext.security.staff} '\n'
-                                	 Admin: ${securityContext.security.admin}`
-                                : 'No Security Context'}
+				{(securityContext) => (
+					<div className="App">
+						<EditForm
+							handle_edit={(event, data) => this.handle_edit(event, data, securityContext.setSecurity)}
+						 	is_edited={this.is_edited}
+						/>
+
+						<h3>
+                            {securityContext.security.edited
+                                ? `Edited${securityContext.security.first_name}`
+                                : 'Please Edit'}
                         </h3>
-                    </div>
-                )}
-            </SecurityContext.Consumer>
-		</div>
-	);
+					</div>
+				)}
+			</SecurityContext.Consumer>
+		);
+	}
 }
+
+export default ProfilePage;
+
+// export default function ProfilePage() {
+// 	return (
+// 		<EditForm />
+		// {/*<div>*/}
+		// {/*	<SecurityContext.Consumer>*/}
+        // {/*        {(securityContext) => (*/}
+        // {/*        	<div>*/}
+        // {/*                <h3>*/}
+        // {/*                    {securityContext.security.logged_in*/}
+        // {/*                        ? 	`Email: ${securityContext.security.email} '\n'*/}
+        // {/*                        	 First Name: ${securityContext.security.first_name} '\n'*/}
+        // {/*                        	 Last Name: ${securityContext.security.last_name} '\n'*/}
+        // {/*                        	 Affiliation: ${securityContext.security.affiliation} '\n'*/}
+        // {/*                        	 Active: ${securityContext.security.active} '\n'*/}
+        // {/*                        	 Staff: ${securityContext.security.staff} '\n'*/}
+        // {/*                        	 Admin: ${securityContext.security.admin}`*/}
+        // {/*                        : 'No Security Context'}*/}
+        // {/*                </h3>*/}
+        // {/*            </div>*/}
+        // {/*        )}*/}
+        // {/*    </SecurityContext.Consumer>*/}
+		// {/*</div>*/}
+// 	);
+// }
--- ChatbotPortal/signup/views.py
+++ ChatbotPortal/signup/views.py
@@ -1,8 +1,10 @@
 from rest_framework import permissions, status
 from rest_framework.decorators import api_view
+from rest_framework.generics import UpdateAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import UserSerializer, UserSerializerWithToken
+from .models import User
 
 
 @api_view(['GET'])
@@ -15,7 +17,7 @@ def current_user(request):
     return Response(serializer.data)
 
 
-class UserList(APIView):
+class UserCreateList(APIView):
     """
     Create a new user. It's called 'UserList' because normally we'd have a get
     method here too, for retrieving a list of all User objects.
@@ -29,3 +31,9 @@ class UserList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserUpdateList(UpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializerWithToken
+    permission_classes = (permissions.AllowAny,)
