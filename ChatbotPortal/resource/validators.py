0c00b194f14911ec6bdc3638bb2f47045b6d563d
TheBurnerGuy <henryweilo@hotmail.com>
2019-10-27 23:33:30 -0600

    Added file size validator

--- ChatbotPortal/resource/validators.py
+++ ChatbotPortal/resource/validators.py
@@ -1,11 +1,14 @@
 from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
-#TODO: actually validate the file size here
 # Validator for making sure that the file doesn't exceed a certain byte range
 # Size should be in mb range
-def validate_file_size(size=100):
+def validate_file_size(size=50):
 
     def internal_validator(value):
-        pass
+        # Value size is in bytes
+        # Reference: https://docs.djangoproject.com/en/2.2/ref/files/uploads/
+        if value.size > size * (10 ** 6):
+            raise ValidationError('Attachment is too large, file must be under {} MB'.format(size))
 
     return internal_validator
