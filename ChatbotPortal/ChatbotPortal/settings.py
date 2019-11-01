9954287acbf3e8a001d9ab6fa0fc8ae64746a4b8
tnguyen10 <tnguyen1@ualberta.ca>
2019-11-01 17:18:57 -0600

    setup: mysql db

--- ChatbotPortal/ChatbotPortal/settings.py
+++ ChatbotPortal/ChatbotPortal/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'ChatbotPortal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db',                  
+        'USER': 'user',             
+        'PASSWORD': 'password',                  
+        'HOST': '',                     
+        'PORT': '', 
     }
 }
 
