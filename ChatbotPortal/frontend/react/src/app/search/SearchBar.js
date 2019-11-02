891d38517b1bdc315e2edb1f8f22299cdedd81a0
apu031 <touhidul.apu@gmail.com>
2019-11-02 08:43:10 -0600

    Commenting searchBar

--- ChatbotPortal/frontend/react/src/app/search/SearchBar.js
+++ ChatbotPortal/frontend/react/src/app/search/SearchBar.js
@@ -1,44 +1,59 @@
 import React, {Component} from 'react';
-import axios from "axios";
 import {SecurityContext} from "../security/SecurityContext";
-import {AutoSizer, Column, InfiniteLoader, Table} from 'react-virtualized';
+import {AutoSizer, Column, SortDirection, Table} from 'react-virtualized';
 import 'react-virtualized/styles.css';
-import PropTypes from "prop-types";
-import LoginForm from "../authentication/LoginForm";
 
-// const {Table, Column} = ReactVirtualized;
-
+/**
+ * This component has a regular view for showing all the users by repeatedly loading data
+ * from the server. Instead of loading everything at once, the view uses virtualized
+ * react table library to fetch the data in a promise resolve manner to ensure the
+ * maximum performance through providing infinite scroll to the user (admin).
+ */
 class SearchBar extends Component {
+
+    // The security context
     static contextType = SecurityContext;
 
     BASE_URL = 'http://127.0.0.1:8000/authentication/';
 
+
     constructor(props) {
         super(props);
+
+        /**
+         * The state of the component
+         */
         this.state = {
-            // users: '',
-            rowHeight:0,
+            rowHeight: 0,
             loadedData: []
         }
     }
 
+    /**
+     * Upon mounting the component, the first row from the database is called,
+     * and stored in the state
+     */
     componentDidMount() {
-        this.loadMoreRows({startIndex: 0,stopIndex: 1});
+        this.loadMoreRows({startIndex: 0, stopIndex: 1});
     }
 
-
+    /**
+     * This function loads more rows upon scrolling down
+     * @param startIndex = the starting row to be fetched from the database
+     * @param stopIndex = the ending row to be fetched from the database
+     */
     loadMoreRows = ({startIndex, stopIndex}) => {
-        this.getRowsFromServer({startIndex, stopIndex}).then( (result) => {
+        this.getRowsFromServer({startIndex, stopIndex}).then((result) => {
             // console.log(result)
             var tempData = this.state.loadedData;
             result.forEach(returnItem => {
                 tempData.push(returnItem)
             });
-            this.setState({loadedData:tempData})
+            this.setState({loadedData: tempData})
         })
     }
 
-    getRowsFromServer = ({startIndex, stopIndex }) => {
+    getRowsFromServer = ({startIndex, stopIndex}) => {
         return new Promise((resolve, reject) => {
             const url = `http://127.0.0.1:8000/authentication/super/rows/${startIndex}/${stopIndex}/`;
 
@@ -50,7 +65,11 @@ class SearchBar extends Component {
 
             axios
                 .get(url, {headers: options})
-                .then(response => {resolve(response.data)}, error => {console.log(error)})
+                .then(response => {
+                    resolve(response.data)
+                }, error => {
+                    console.log(error)
+                })
         })
     };
 
@@ -60,8 +79,13 @@ class SearchBar extends Component {
     };
 
 
+    sortDirection = () => {
+    }
+    sortBy = () => {
+    }
+
     render() {
-        return(
+        return (
             <div className="container">
                 <h1>Users</h1>
                 <InfiniteLoader
@@ -74,88 +98,94 @@ class SearchBar extends Component {
                     rowCount={1000000}
                 >
                     {/*onRowsRender: This function should be passed as the child's onRowsRender property,
-                    it informs loader when the user is scrolling*/}
-                    {/*registerChild: This function should be set as the child's ref property. It enables a set
-                    of rows to be refreshed once their data has finished loading*/}
-                    {({onRowsRendered, registerChild}) => (
-                        <AutoSizer>
-                            {({width}) =>
-                                <Table
-                                    ref={registerChild}
-                                    onRowsRendered={onRowsRendered}
-                                    rowClassName='table-row'
-                                    // The height of the table header
-                                    headerHeight={40}
-                                    // The width of the table
-                                    width={width}
-                                    // The height of the table
-                                    height={400}
-                                    // The height of the rows
-                                    rowHeight={40}
-                                    // The number of rows (real time is expensive operation)
-                                    rowCount={this.state.loadedData.length}
-                                    // A function that given the row index returns the rwo object
-                                    rowGetter={({index}) => this.state.loadedData[index]}
-                                >
-                                    <Column
-                                        label='Id'
-                                        // The key name of the row object used to retrieve the value inserted in the cell
-                                        dataKey='id'
-                                        // The width of the column
-                                        width={width * 0.1}
-                                    />
-                                    <Column
-                                        label='Email'
-                                        dataKey='email'
-                                        width={width * 0.1}
-                                    />
-                                    <Column
-                                        label='First Name'
-                                        dataKey='first_name'
-                                        width={width * 0.1}
-                                    />
-                                    <Column
-                                        label='Last Name'
-                                        dataKey='last_name'
-                                        width={width * 0.1}
-                                    />
-                                    <Column
-                                        label='Joined on'
-                                        dataKey='date_joined'
-                                        width={width * 0.1}
-                                    />
-                                    <Column
-                                        label='Last Login'
-                                        dataKey='last_login'
-                                        width={width * 0.1}
-                                    />
-                                    <Column
-                                        label='Activated'
-                                        dataKey='is_active'
-                                        width={width * 0.1}
-                                    />
-                                    <Column
-                                        label='Reviewer'
-                                        dataKey='is_reviewer'
-                                        width={width * 0.1}
-                                    />
-                                    <Column
-                                        label='Staff'
-                                        dataKey='is_staff'
-                                        width={width * 0.1}
-                                    />
-                                    <Column
-                                        label='Admin'
-                                        dataKey='is_superuser'
-                                        width={width * 0.1}
-                                    />
-                                    <Column
-                                        label='Affiliation'
-                                        dataKey='affiliation'
-                                        width={width * 0.1}
-                                    />
-                                </Table>}
-                        </AutoSizer>)}
+                    it informs loader when the user is scrolling*/
+                    } {/*registerChild: This function should be set as the child's ref property. It enables a set
+                    of rows to be refreshed once their data has finished loading*/
+                }
+
+                    {
+                        ({onRowsRendered, registerChild}) => (
+                            <AutoSizer>
+                                {({width}) =>
+                                    <Table
+                                        ref={registerChild}
+                                        onRowsRendered={onRowsRendered}
+                                        sortBy={'first_name'}
+                                        sortDirection={SortDirection.ASC}
+                                        rowClassName='table-row'
+                                        // The height of the table header
+                                        headerHeight={40}
+                                        // The width of the table
+                                        width={width}
+                                        // The height of the table
+                                        height={400}
+                                        // The height of the rows
+                                        rowHeight={40}
+                                        // The number of rows (real time is expensive operation)
+                                        rowCount={this.state.loadedData.length}
+                                        // A function that given the row index returns the rwo object
+                                        rowGetter={({index}) => this.state.loadedData[index]}
+                                    >
+                                        <Column
+                                            label='Id'
+                                            // The key name of the row object used to retrieve the value inserted in the cell
+                                            dataKey='id'
+                                            // The width of the column
+                                            width={width * 0.1}
+                                        />
+                                        <Column
+                                            label='Email'
+                                            dataKey='email'
+                                            width={width * 0.1}
+                                        />
+                                        <Column
+                                            label='First Name'
+                                            dataKey='first_name'
+                                            width={width * 0.1}
+                                        />
+                                        <Column
+                                            label='Last Name'
+                                            dataKey='last_name'
+                                            width={width * 0.1}
+                                        />
+                                        <Column
+                                            label='Joined on'
+                                            dataKey='date_joined'
+                                            width={width * 0.1}
+                                        />
+                                        <Column
+                                            label='Last Login'
+                                            dataKey='last_login'
+                                            width={width * 0.1}
+                                        />
+                                        <Column
+                                            label='Activated'
+                                            dataKey='is_active'
+                                            width={width * 0.1}
+                                        />
+                                        <Column
+                                            label='Reviewer'
+                                            dataKey='is_reviewer'
+                                            width={width * 0.1}
+                                        />
+                                        <Column
+                                            label='Staff'
+                                            dataKey='is_staff'
+                                            width={width * 0.1}
+                                        />
+                                        <Column
+                                            label='Admin'
+                                            dataKey='is_superuser'
+                                            width={width * 0.1}
+                                        />
+                                        <Column
+                                            label='Affiliation'
+                                            dataKey='affiliation'
+                                            width={width * 0.1}
+                                        />
+                                    </Table>}
+                            </AutoSizer>)
+                    }
                 </InfiniteLoader>
             </div>
         );
@@ -163,3 +193,6 @@ class SearchBar extends Component {
 }
 
 export default SearchBar;
+
+//https://medium.com/@joedister/using-react-virtualized-infiniteloader-autosizer-and-table-with-material-ui-styles-react-76d3596b6c93
+//https://www.abidibo.net/blog/2019/01/11/react-virtualized-infinite-scrolling-table-how/
