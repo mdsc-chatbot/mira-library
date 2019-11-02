1204e8c3e6ebecb52b1ab1145d49c26f74df11dc
apu031 <touhidul.apu@gmail.com>
2019-11-02 00:04:24 -0600

    total_users is removed from searchPage

--- ChatbotPortal/frontend/react/src/app/search/SearchPage.js
+++ ChatbotPortal/frontend/react/src/app/search/SearchPage.js
@@ -8,46 +8,9 @@ import axios from "axios";
 class SearchPage extends Component {
     constructor(props) {
         super(props);
-
-        this.state = {
-            total_users: ''
-        }
     }
 
     static contextType = SecurityContext;
-
-    componentDidMount() {
-        this.get_total_user()
-    }
-
-    get_total_user = () => {
-        const url = `http://127.0.0.1:8000/authentication/super/total/users/`;
-
-        // Having the permission header loaded
-        const options = {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${this.context.security.token}`
-        };
-
-        /**
-         * Calling the backend API
-         */
-        axios
-            .get(url, {headers: options})
-            .then(
-                response => {
-                    console.log(response.data);
-                    this.setState({
-
-                        // Setting the response in user state
-                        total_users: response.data['user_count'],
-                    });
-                },
-                error => {
-                    console.log(error);
-                }
-            )
-    };
 
     render() {
         return (
@@ -65,7 +28,7 @@ class SearchPage extends Component {
                     Search
                 </Header>
                 <SearchAdvancedOption />
-                <SearchBar total_number_of_user = {this.state.total_users}/>
+                <SearchBar />
             </div>
         );
     }
