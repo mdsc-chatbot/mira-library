143e76458e1f63829a803a49f97ab7fab7fd3498
tnguyen10 <tnguyen1@ualberta.ca>
2019-11-03 17:39:22 -0700

    test: remove fix position from footer that messes with selenium test, find tag postion

--- ChatbotPortal/frontend/react/src/app/Footer.js
+++ ChatbotPortal/frontend/react/src/app/Footer.js
@@ -4,7 +4,7 @@ import { Container, Grid, Segment } from "semantic-ui-react";
 export class Footer extends Component {
     render() {
         return (
-            <div style={{position:"absolute",bottom:0,left:0,right:0}}>
+            <div>
                 <Segment vertical inverted>
                     <Grid container stackable verticalAlign="middle">
                         <Grid.Row divided>
