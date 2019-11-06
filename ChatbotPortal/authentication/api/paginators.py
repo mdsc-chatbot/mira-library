8bacd4dc66951501181afa3af938acfcebb1da47
apu031 <touhidul.apu@gmail.com>
2019-11-06 08:00:26 -0700

    Pagination implementation

---
+++ ChatbotPortal/authentication/api/paginators.py
@@ -0,0 +1,6 @@
+from rest_framework.pagination import PageNumberPagination
+
+class ChatBotPaginator(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = 'page_size'
+    max_page_size = 100000
