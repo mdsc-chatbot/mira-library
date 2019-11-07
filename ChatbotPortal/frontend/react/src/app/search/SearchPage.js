b01618e327c33416cd793de6e399a524898590d8
apu031 <touhidul.apu@gmail.com>
2019-11-07 12:50:04 -0700

    SearchPage is updated

--- ChatbotPortal/frontend/react/src/app/search/SearchPage.js
+++ ChatbotPortal/frontend/react/src/app/search/SearchPage.js
@@ -3,17 +3,12 @@ import SearchTable from "./SearchTable";
 import SearchAdvancedOption from "./SearchAdvancedOption";
 import {Button, Header} from "semantic-ui-react";
 import {SecurityContext} from "../security/SecurityContext";
-import axios from "axios";
 import SearchByAnything from "./SearchByAnything";
-import SearchByDateRange from "./SearchByDateRange";
-import SearchFilter from "./SearchFilter";
-import SearchByIdRange from "./SearchByIdRange";
 
 class SearchPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            show_all_user: true,
             loadedData: [],
 
             is_active: "''",
@@ -28,9 +23,9 @@ class SearchPage extends Component {
             start_id: "''",
             end_id: "''",
 
-            search_string: "''",
+            search_string: '',
 
-            url: "''"
+            url: "http://127.0.0.1:8000/authentication/super/search/status/''/''/''/''/date_range/''/''/''/id_range/''/''/search_value/?search="
         };
     }
 
@@ -62,15 +57,7 @@ class SearchPage extends Component {
     submit_query = (e) => {
         e.preventDefault();
         this.setState({
-            show_all_user: false,
-        });
-        // console.log(`http://127.0.0.1:8000/authentication/super/search/status/${this.state.is_active}/${this.state.is_reviewer}/${this.state.is_staff}/${this.state.is_superuser}/date_range/${this.state.search_option}/${this.state.start_date}/${this.state.end_date}/id_range/${this.state.start_id}/${this.state.end_id}/search_value/?search=${this.state.search_string}`)
-    };
-
-    handle_result_change = (results) => {
-        this.setState({
-            results,
-            is_advance_used : true
+            url: `http://127.0.0.1:8000/authentication/super/search/status/${this.state.is_active}/${this.state.is_reviewer}/${this.state.is_staff}/${this.state.is_superuser}/date_range/${this.state.search_option}/${this.state.start_date}/${this.state.end_date}/id_range/${this.state.start_id}/${this.state.end_id}/search_value/?search=${this.state.search_string}`
         });
     };
 
@@ -91,13 +78,13 @@ class SearchPage extends Component {
                 >
                     Search
                 </Header>
-                {/*<SearchByDateRange set_date_range_params={this.set_date_range_params} set_date_option_params={this.set_date_option_params}/>*/}
-                {/*<SearchFilter set_status_search_params={this.set_status_search_params}/>*/}
-                {/*<SearchByIdRange set_id_search_params={this.set_id_search_params} />*/}
-                <SearchByAnything set_search_string = {this.set_search_string}/>
-                <SearchAdvancedOption set_date_range_params={this.set_date_range_params} set_date_option_params={this.set_date_option_params} set_status_search_params={this.set_status_search_params} set_id_search_params={this.set_id_search_params}/>
+                <SearchByAnything set_search_string={this.set_search_string}/>
+                <SearchAdvancedOption set_date_range_params={this.set_date_range_params}
+                                      set_date_option_params={this.set_date_option_params}
+                                      set_status_search_params={this.set_status_search_params}
+                                      set_id_search_params={this.set_id_search_params}/>
                 <Button color="blue" fluid size="large" onClick={this.submit_query}>Search</Button>
-                <SearchTable loadedData = {this.state.loadedData} is_advance_used = {this.state.is_advance_used}/>
+                <SearchTable url={this.state.url}/>
             </div>
         );
     }
