66446f03b4b79939ab02676529992e895c77a35e
apu031 <touhidul.apu@gmail.com>
2019-11-11 17:43:45 -0700

    Background chrome.js is nicely documented

--- ChatbotPortal/frontend/extension/lib/chrome/chrome.js
+++ ChatbotPortal/frontend/extension/lib/chrome/chrome.js
@@ -1,33 +1,46 @@
 let app = {}, topurl = {};
 
+/**
+ * Getting the version from the manifest
+ * @returns {string}
+ */
 app.version = function () {
     return chrome.runtime.getManifest().version
 };
+
+/**
+ * Getting the homepage url from the manifest
+ * @returns {string}
+ */
 app.homepage = function () {
     return chrome.runtime.getManifest().homepage_url
 };
+
+/**
+ * Setting up app uninstall status
+ */
 chrome.runtime.setUninstallURL(app.homepage() + "?v=" + app.version() + "&type=uninstall", function () {
 });
 
-app.tab = {
-    "openOptions": function () {
-        chrome.runtime.openOptionsPage()
-    },
-    "open": function (url) {
-        chrome.tabs.create({"url": url, "active": true})
-    }
-};
-
-chrome.runtime.onInstalled.addListener(function (e) {
+/**
+ * Setting up app installation status
+ */
+chrome.runtime.onInstalled.addListener(function (event) {
     window.setTimeout(function () {
-        if (e.reason === "install") {
-            app.tab.open(app.homepage() + "?v=" + app.version() + "&type=" + e.reason);
+        if (event.reason === "install") {
+            //  Upon installing for the first time, redirect to the ChatbotPortal homepage
+            app.tab.open(app.homepage() + "?v=" + app.version() + "&type=" + event.reason);
         }
     }, 3000);
 });
 
+/**
+ * Setting up the app storage with required data
+ * @type {{read: (function(*): *), write: app.storage.write}}
+ */
 app.storage = (function () {
     let objs = {};
+    // Injecting common.js script
     window.setTimeout(function () {
         chrome.storage.local.get(null, function (o) {
             objs = o;
@@ -36,103 +49,104 @@ app.storage = (function () {
             document.body.appendChild(script);
         });
     }, 300);
-    /*  */
+
     return {
+        // Reading from the storage
         "read": function (id) {
             return objs[id]
         },
+
+        // Writing in the local storage
         "write": function (id, data) {
-            let tmp = {};
-            tmp[id] = data;
+            let temporary = {};
+            temporary[id] = data;
             objs[id] = data;
-            chrome.storage.local.set(tmp, function () {
+            chrome.storage.local.set(temporary, function () {
             });
         }
     }
 })();
 
+/**
+ * Initializing the popup communication object
+ * that the background will use to interact with the popup
+ * @type {{receive: app.popup.receive, send: app.popup.send}}
+ */
 app.popup = (function () {
-    let tmp = {};
+    let temporary = {};
+    // Setting up the communication ground from popup to background
     chrome.runtime.onMessage.addListener(function (request) {
-        for (let id in tmp) {
-            if (tmp[id] && (typeof tmp[id] === "function")) {
-                if (request.path === "popup-to-background") {
-                    if (request.method === id) tmp[id](request.data);
+        for (let id in temporary) {
+            if (temporary.hasOwnProperty(id)) {
+                if (temporary[id] && (typeof temporary[id] === "function")) {
+                    if (request.path === "popup-to-background") {
+                        if (request.method === id) temporary[id](request.data);
+                    }
                 }
             }
         }
     });
-    /*  */
+
     return {
+        // Receiving information from the popup upon sending query
         "receive": function (id, callback) {
-            tmp[id] = callback
+            temporary[id] = callback
         },
+        // Sending the query to the popup for a required method and data
         "send": function (id, data) {
             chrome.runtime.sendMessage({"path": "background-to-popup", "method": id, "data": data});
         }
     }
 })();
 
-app.options = (function () {
-    let tmp = {};
-    chrome.runtime.onMessage.addListener(function (request) {
-        for (let id in tmp) {
-            if (tmp[id] && (typeof tmp[id] === "function")) {
-                if (request.path === "options-to-background") {
-                    if (request.method === id) tmp[id](request.data);
-                }
-            }
-        }
-    });
-    /*  */
-    return {
-        "receive": function (id, callback) {
-            tmp[id] = callback
-        },
-        "send": function (id, data) {
-            chrome.runtime.sendMessage({"path": "background-to-options", "method": id, "data": data});
-        }
-    }
-})();
-
+/**
+ * Fires when a request is about to occur.
+ * This event is sent before any TCP connection is made and can be used to cancel or redirect requests.
+ * @param callback = Being called after asynchronous operation
+ */
 app.onBeforeRequest = function (callback) {
     let onBeforeRequest = function (info) {
         if (info.tabId > -1) return;
-        /*  */
         if (info.url.indexOf("http") === 0) {
             let id = info.tabId + '|' + (info.parentFrameId || info.frameId);
             if (info.type === 'main_frame' || info.type === "sub_frame") topurl[id] = info.url;
             return callback(topurl[id], info.url);
         }
     };
-    /*  */
     chrome.webRequest.onBeforeRequest.addListener(onBeforeRequest, {"urls": ["<all_urls>"]}, ["blocking"]);
 };
 
+/**
+ * Fires when a request is about to occur and the initial headers have been prepared.
+ * The event is intended to allow extensions to add, modify, and delete request headers.
+ * @param callback = Being called after asynchronous operation
+ */
 app.onBeforeSendHeaders = function (callback) {
-    let onBeforeSendHeaders = function (info) {
-        if (info.tabId > -1) return;
-        /*  */
-        if (info.url.indexOf("http") === 0) {
-            let id = info.tabId + '|' + (info.parentFrameId || info.frameId);
-            if (info.type === 'main_frame' || info.type === "sub_frame") topurl[id] = info.url;
-            return callback(topurl[id], info.url, info.requestHeaders);
+    let onBeforeSendHeaders = function (information) {
+        if (information.tabId > -1) return;
+        if (information.url.indexOf("http") === 0) {
+            let id = information.tabId + '|' + (information.parentFrameId || information.frameId);
+            if (information.type === 'main_frame' || information.type === "sub_frame") topurl[id] = information.url;
+            return callback(topurl[id], information.url, information.requestHeaders);
         }
     };
-    /*  */
     chrome.webRequest.onBeforeSendHeaders.addListener(onBeforeSendHeaders, {"urls": ["<all_urls>"]}, ["blocking", "requestHeaders"]);
 };
 
+/**
+ * Fires each time that an HTTP(S) response header is received.
+ * Due to redirects and authentication requests this can happen multiple times per request.
+ * This event is intended to allow extensions to add, modify, and delete response headers, such as incoming Content-Type headers.
+ * @param callback = Being called after asynchronous operation
+ */
 app.onHeadersReceived = function (callback) {
-    let onHeadersReceived = function (info) {
-        if (info.tabId > -1) return;
-        /*  */
-        if (info.url.indexOf("http") === 0) {
-            var id = info.tabId + '|' + (info.parentFrameId || info.frameId);
-            if (info.type === 'main_frame' || info.type === "sub_frame") topurl[id] = info.url;
-            return callback(topurl[id], info.url, info.responseHeaders);
+    let onHeadersReceived = function (information) {
+        if (information.tabId > -1) return;
+        if (information.url.indexOf("http") === 0) {
+            let id = information.tabId + '|' + (information.parentFrameId || information.frameId);
+            if (information.type === 'main_frame' || information.type === "sub_frame") topurl[id] = information.url;
+            return callback(topurl[id], information.url, information.responseHeaders);
         }
     };
-    /*  */
     chrome.webRequest.onHeadersReceived.addListener(onHeadersReceived, {"urls": ["<all_urls>"]}, ["blocking", "responseHeaders"]);
 };
