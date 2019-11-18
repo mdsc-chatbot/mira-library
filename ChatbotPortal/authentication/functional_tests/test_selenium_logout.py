9a80bb59a5eac46120dd1174da537c05c15e99cb
apu031 <touhidul.apu@gmail.com>
2019-11-17 17:23:25 -0700

    Logout selenium test is done

--- ChatbotPortal/authentication/functional_tests/test_selenium_logout.py
+++ ChatbotPortal/authentication/functional_tests/test_selenium_logout.py
@@ -5,10 +5,10 @@ from selenium import webdriver
 
 from ..models import CustomUser
 
-# The base url
-BASE_URL = 'http://127.0.0.1:8000'
-url = BASE_URL + '/chatbotportal/app/login'
-homepage = BASE_URL + '/chatbotportal/app'
+LOGIN_PAGE = '/chatbotportal/app/login'
+HOME_PAGE = '/chatbotportal/app'
+
+WAIT_SECONDS = 5
 
 
 class TestLogout(LiveServerTestCase):
@@ -57,16 +57,101 @@ class TestLogout(LiveServerTestCase):
         """
         CustomUser.objects.all().delete()
 
-    def test_logout_is_hidden_from_not_logged_in_users(self):
+    def test_logout_menu_is_hidden_to_a_public_user(self):
         """
         Test public users should not be able to logout button
         :return: None
         """
 
-        self.browser.get('%s%s' % (self.live_server_url, '/chatbotportal/app'))
-        # self.browser.get(homepage)
-        time.sleep(1)
+        self.browser.get('%s%s' % (self.live_server_url, HOME_PAGE))
+        time.sleep(WAIT_SECONDS)
 
-        # Finding the login button
-        logout_button = self.browser.find_elements_by_name('Logout')
-        self.assertEqual(len(logout_button), 0)
+        # Finding the logout button
+        logout_menu = self.browser.find_elements_by_link_text('Logout')
+        self.assertEqual(len(logout_menu), 0)
+
+    def test_logout_menu_is_visble_to_a_logged_in_user(self):
+        """
+        Test a logged in user can see the logout button
+        :return: None
+        """
+
+        self.browser.get('%s%s' % (self.live_server_url, HOME_PAGE))
+        time.sleep(WAIT_SECONDS)
+
+        # Find the login menu
+        login_menu = self.browser.find_element_by_link_text('Login')
+        self.assertIsNotNone(login_menu)
+        login_menu.click()
+
+        time.sleep(WAIT_SECONDS)
+
+        # Logging in
+        self.browser.find_element_by_name('email').send_keys(self.active_user_email)
+        self.browser.find_element_by_name('password').send_keys(self.active_user_password)
+        self.browser.find_element_by_name('login_button').click()
+
+        time.sleep(WAIT_SECONDS)
+
+        # The user is redirected to the homepage
+        self.assertURLEqual(self.live_server_url + HOME_PAGE, self.browser.current_url)
+
+        # Login should not be visible anymore menu
+        login_menu = self.browser.find_elements_by_link_text('Login')
+        self.assertEqual(len(login_menu), 0)
+
+        # Logout menu should be visible now
+        logout_menu = self.browser.find_element_by_link_text('Logout')
+        self.assertIsNotNone(logout_menu)
+
+        time.sleep(WAIT_SECONDS)
+
+    def test_logout_successfully(self):
+        """
+        Test a logged in user can see the logout button
+        :return: None
+        """
+
+        self.browser.get('%s%s' % (self.live_server_url, HOME_PAGE))
+        time.sleep(WAIT_SECONDS)
+
+        # Logging in
+        self.browser.find_element_by_link_text('Login').click()
+
+        time.sleep(WAIT_SECONDS)
+
+        # Logging in
+        self.browser.find_element_by_name('email').send_keys(self.active_user_email)
+        self.browser.find_element_by_name('password').send_keys(self.active_user_password)
+        self.browser.find_element_by_name('login_button').click()
+
+        time.sleep(WAIT_SECONDS)
+
+        # The user is redirected to the homepage
+        self.assertURLEqual(self.live_server_url + HOME_PAGE, self.browser.current_url)
+
+        # Login should not be visible anymore menu
+        login_menu = self.browser.find_elements_by_link_text('Login')
+        self.assertEqual(len(login_menu), 0)
+
+        # Logout menu should be visible now
+        logout_menu = self.browser.find_element_by_link_text('Logout')
+        self.assertIsNotNone(logout_menu)
+
+        # Logging out
+        logout_menu.click()
+
+        time.sleep(WAIT_SECONDS)
+
+        # The user is redirected to the homepage
+        self.assertURLEqual(self.live_server_url + HOME_PAGE, self.browser.current_url)
+
+        # Logout should be invisible again
+        logout_menu = self.browser.find_elements_by_link_text('Logout')
+        self.assertEqual(len(logout_menu), 0)
+
+        # Login should be visible again
+        login_menu = self.browser.find_elements_by_link_text('Login')
+        self.assertEqual(len(login_menu), 1)
+
+        time.sleep(WAIT_SECONDS)
