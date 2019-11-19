87ada3de077ccb4c811da9afc613612aded3d69b
RitvikKhanna <ritvik@ualberta.ca>
2019-11-19 16:16:29 -0700

    Mobile UI

--- ChatbotPortal/frontend/react/src/app/Footer.js
+++ ChatbotPortal/frontend/react/src/app/Footer.js
@@ -1,17 +1,21 @@
 import React, { Component } from "react";
-import { Container, Grid, Segment } from "semantic-ui-react";
+import { Icon, Grid, Segment } from "semantic-ui-react";
 
 export class Footer extends Component {
     render() {
         return (
             <div>
                 <Segment inverted attached='bottom'>
-                    <Grid container stackable verticalAlign="middle">
-                        <Grid.Row divided>
-                            <Grid.Column>About </Grid.Column>
-                            <Grid.Column>Contact </Grid.Column>
+                    <Grid container stackable verticalAlign='middle'>
+                        <Grid.Row>
+                            <Grid.Column>About</Grid.Column>
+                            <Grid.Column>Contact</Grid.Column>
                         </Grid.Row>
-                        <Grid.Row>© 2019 Powered by Chatbot Resources</Grid.Row>
+                        <Grid.Row>
+                            <Grid.Column>
+                                <Icon name='copyright outline' />Powered by Chatbot Resources
+                            </Grid.Column>
+                        </Grid.Row>
                     </Grid>
                 </Segment>
             </div>
