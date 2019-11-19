f8294ce36d7e59b296b2b8677c393f758f63264b
tnguyen10 <tnguyen1@ualberta.ca>
2019-11-18 18:06:40 -0700

    resource: clean up and add more test to resource unit test

--- ChatbotPortal/resource/tests/tests.py
+++ ChatbotPortal/resource/tests/tests.py
@@ -5,38 +5,36 @@ from ..models import Resource, Category
 
 
 class ResourceTest(TestCase):
+
+    def create_resource(self, url):
+        category = Category(id=1,name="website")
+        resource = Resource.objects.create(
+            url=url,
+            category=category,
+        )
+        return resource
+
     def test_webscrape_website_title(self):
         Resource.objects.all().delete()
-        category = Category(id=1, name="website")
-        resource = Resource.objects.create(
-            title="Unknown title",
-            url="https://www.ualberta.ca/",
-            rating=1,
-            comments="",
-            created_by_user="Unknown user",
-            created_by_user_pk=-1,
-            category=category,
-
-        )
+        resource = self.create_resource("https://myhealth.alberta.ca/")
         resource.save()
         db_resource = Resource.objects.get(pk=1)
-        self.assertTrue("University of Alberta" in db_resource.title.strip())
+        self.assertTrue("MyHealth.Alberta.ca" in db_resource.title.strip())
+
+        resource = self.create_resource("https://thisisaninvalid.com/")
+        resource.save()
+        db_resource = Resource.objects.get(pk=1)
+        self.assertTrue("" in db_resource.title.strip())
 
     def test_validations(self):
         Resource.objects.all().delete()
-        category = Category(id=1, name="website")
-        resource = Resource.objects.create(
-            title="Unknown title",
-            url="https://www.ualberta.ca/",
-            rating=1,
-            comments="",
-            created_by_user="Unknown user",
-            created_by_user_pk=-1,
-            category=category
-        )
+        resource = self.create_resource("https://myhealth.alberta.ca/")
         resource.full_clean()
 
-        resource.url = "Invalid url"
+        resource.url = "this_is_an_invalid_url"
+        self.assertRaises(ValidationError, resource.full_clean)
+
+        resource.url = "www.thisisaninvalidurl.com"
         self.assertRaises(ValidationError, resource.full_clean)
 
         resource.rating = 10
