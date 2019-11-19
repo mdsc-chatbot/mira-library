2563ed8a15a8bec5135551fe3a209b77cc6daf49
RitvikKhanna <ritvik@ualberta.ca>
2019-11-19 01:26:10 -0700

    cosmetic mobile UI changes

--- ChatbotPortal/frontend/react/src/app/HeaderMenu.js
+++ ChatbotPortal/frontend/react/src/app/HeaderMenu.js
@@ -127,116 +127,116 @@ export class HeaderMenu extends Component {
     headerMenuMobile = () => {
         const { activeItem } = this.state;
         return(
-            <div>
-                <Segment>
-                    <Menu fluid widths = {2} size="small">
-                        <Menu.Item
-                            as="a"
-                            style={{ paddingLeft: 50 }}
-                            active={activeItem === "Home"}
-                            onClick={this.handleItemClick}
-                        >
-                            <Link to={baseRoute}>
-                                <Header as="h4" style={{ color: "#3075c9" }}>
-                                    <Icon name="qq" />
-                                    Chatbot Resources
-                                </Header>
-                            </Link>
-                        </Menu.Item>
-                        <Menu.Menu position = 'right'>
+            //<div>
+              //  {/*    <Segment>*/}
+                <Menu fluid widths = {2} size="small">
+                    <Menu.Item
+                        as="a"
+                        style={{ paddingLeft: 50 }}
+                        active={activeItem === "Home"}
+                        onClick={this.handleItemClick}
+                    >
+                        <Link to={baseRoute}>
+                            <Header as="h4" style={{ color: "#3075c9" }}>
+                                <Icon name="qq" />
+                                Chatbot Resources
+                            </Header>
+                        </Link>
+                    </Menu.Item>
+                    <Menu.Menu position = 'right'>
 
-                            <Dropdown item text='Menu' floating labeled>
+                        <Dropdown item text='Menu' floating labeled>
 
-                                <Dropdown.Menu>
+                            <Dropdown.Menu>
 
-                                    <Dropdown.Item>
-                                        <Menu.Item
-                                            name="Public Resources"
-                                            as={Link}
-                                            to={baseRoute + "/public_resource"}
-                                            position="right"
-                                            active={activeItem === "Public Resources"}
-                                            onClick={this.handleItemClick}
-                                        />
-                                    </Dropdown.Item>
+                                <Dropdown.Item>
+                                    <Menu.Item
+                                        name="Public Resources"
+                                        as={Link}
+                                        to={baseRoute + "/public_resource"}
+                                        position="right"
+                                        active={activeItem === "Public Resources"}
+                                        onClick={this.handleItemClick}
+                                    />
+                                </Dropdown.Item>
 
-                                    {this.context.security.is_logged_in && (
-                                        <Dropdown.Item><Menu.Item
-                                            name="My Profile"
-                                            as={Link}
-                                            to={baseRoute + "/profile"}
-                                            active={activeItem === "My Profile"}
-                                            onClick={this.handleItemClick}
-                                        /></Dropdown.Item>
-                                    )}
+                                {this.context.security.is_logged_in && (
+                                    <Dropdown.Item><Menu.Item
+                                        name="My Profile"
+                                        as={Link}
+                                        to={baseRoute + "/profile"}
+                                        active={activeItem === "My Profile"}
+                                        onClick={this.handleItemClick}
+                                    /></Dropdown.Item>
+                                )}
 
-                                    {this.context.security.is_logged_in && (
-                                        <Dropdown.Item><Menu.Item
-                                            name="My resources"
-                                            as={Link}
-                                            to={baseRoute + "/resource"}
-                                            active={activeItem === "My resources"}
-                                            onClick={this.handleItemClick}
-                                        /></Dropdown.Item>
-                                    )}
+                                {this.context.security.is_logged_in && (
+                                    <Dropdown.Item><Menu.Item
+                                        name="My resources"
+                                        as={Link}
+                                        to={baseRoute + "/resource"}
+                                        active={activeItem === "My resources"}
+                                        onClick={this.handleItemClick}
+                                    /></Dropdown.Item>
+                                )}
 
-                                    {this.context.security.is_logged_in && (
-                                        <Dropdown.Item><Menu.Item
-                                            name="My reviews"
-                                            as={Link}
-                                            to={baseRoute + "/review"}
-                                            active={activeItem === "My reviews"}
-                                            onClick={this.handleItemClick}
-                                        /></Dropdown.Item>
-                                    )}
+                                {this.context.security.is_logged_in && (
+                                    <Dropdown.Item><Menu.Item
+                                        name="My reviews"
+                                        as={Link}
+                                        to={baseRoute + "/review"}
+                                        active={activeItem === "My reviews"}
+                                        onClick={this.handleItemClick}
+                                    /></Dropdown.Item>
+                                )}
 
-                                    {this.context.security.is_logged_in && this.context.security.is_staff && (
-                                        <Dropdown.Item><Menu.Item
-                                            name="Search"
-                                            as={Link}
-                                            to={baseRoute + "/search"}
-                                            active={activeItem === "Search"}
-                                            onClick={this.handleItemClick}
-                                        /></Dropdown.Item>
-                                    )}
+                                {this.context.security.is_logged_in && this.context.security.is_staff && (
+                                    <Dropdown.Item><Menu.Item
+                                        name="Search"
+                                        as={Link}
+                                        to={baseRoute + "/search"}
+                                        active={activeItem === "Search"}
+                                        onClick={this.handleItemClick}
+                                    /></Dropdown.Item>
+                                )}
 
-                                    {this.context.security.is_logged_in && (
-                                        <Dropdown.Item><Menu.Item
-                                            name="Logout"
-                                            as={Link}
-                                            to={baseRoute + "/logout"}
-                                            active={activeItem === "Logout"}
-                                            onClick={this.handleItemClick}
-                                        /></Dropdown.Item>
-                                    )}
+                                {this.context.security.is_logged_in && (
+                                    <Dropdown.Item><Menu.Item
+                                        name="Logout"
+                                        as={Link}
+                                        to={baseRoute + "/logout"}
+                                        active={activeItem === "Logout"}
+                                        onClick={this.handleItemClick}
+                                    /></Dropdown.Item>
+                                )}
 
-                                    {!this.context.security.is_logged_in && (
-                                        <Dropdown.Item><Menu.Item
-                                            name="Login"
-                                            as={Link}
-                                            to={baseRoute + "/login"}
-                                            active={activeItem === "Login"}
-                                            onClick={this.handleItemClick}
-                                        /></Dropdown.Item>
-                                    )}
+                                {!this.context.security.is_logged_in && (
+                                    <Dropdown.Item><Menu.Item
+                                        name="Login"
+                                        as={Link}
+                                        to={baseRoute + "/login"}
+                                        active={activeItem === "Login"}
+                                        onClick={this.handleItemClick}
+                                    /></Dropdown.Item>
+                                )}
 
-                                    <Dropdown.Item>
-                                        <Menu.Item
-                                            name="FAQ"
-                                            as={Link}
-                                            to={baseRoute + "/faq"}
-                                            active={activeItem === "FAQ"}
-                                            onClick={this.handleItemClick}
-                                        />
-                                    </Dropdown.Item>
-                                </Dropdown.Menu>
+                                <Dropdown.Item>
+                                    <Menu.Item
+                                        name="FAQ"
+                                        as={Link}
+                                        to={baseRoute + "/faq"}
+                                        active={activeItem === "FAQ"}
+                                        onClick={this.handleItemClick}
+                                    />
+                                </Dropdown.Item>
+                            </Dropdown.Menu>
 
-                            </Dropdown>
+                        </Dropdown>
 
-                        </Menu.Menu>
-                    </Menu>
-                </Segment>
-            </div>
+                    </Menu.Menu>
+                </Menu>
+                //     </Segment>
+           // </div>
 
         );
     };
