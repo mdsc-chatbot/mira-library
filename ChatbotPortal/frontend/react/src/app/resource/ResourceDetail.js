3a9701d2b33da69fbbc95e721d6fc1260999f444
tnguyen10 <tnguyen1@ualberta.ca>
2019-11-20 17:00:09 -0700

    resource: trying to figure out grid dimension for mobile

--- ChatbotPortal/frontend/react/src/app/resource/ResourceDetail.js
+++ ChatbotPortal/frontend/react/src/app/resource/ResourceDetail.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
-import {ResourceDetailView} from '../shared';
+import { ResourceDetailView } from "../shared";
 import { SecurityContext } from "../security/SecurityContext";
 
 export default class ResourceDetail extends Component {
@@ -29,7 +29,18 @@ export default class ResourceDetail extends Component {
 
     render() {
         return (
-            <ResourceDetailView resource={this.state.resource} />
+            <div>
+                <Responsive as={Segment} {...Responsive.onlyMobile}>
+                    <div style={{ paddingTop: 30, paddingLeft: 15, paddingRight: 15, paddingBottom: 30 }}>
+                        <ResourceDetailView resource={this.state.resource} />
+                    </div>
+                </Responsive>
+                <Responsive as={Segment} minWidth={768}>
+                    <div style={{ paddingTop: 30, paddingLeft: 100, paddingRight: 100, paddingBottom: 30 }}>
+                        <ResourceDetailView resource={this.state.resource} />
+                    </div>
+                </Responsive>
+            </div>
         );
     }
 }
--- ChatbotPortal/frontend/react/src/app/shared/ResourceDetailView.js
+++ ChatbotPortal/frontend/react/src/app/shared/ResourceDetailView.js
@@ -15,12 +15,15 @@ function normal_grid_element(grid_key, grid_value) {
     const gridKeyColumnProps = {
         className: styles.greyColumn,
         floated: "left",
-        textAlign: "right",
-        width: "2"
+        mobile: "8",
+        tablet: "2",
+        computer: "2"
     };
 
     const gridValueColumnProps = {
-        width: "14"
+        mobile: "8",
+        tablet: "14",
+        computer: "14"
     };
 
     return (
@@ -30,90 +33,90 @@ function normal_grid_element(grid_key, grid_value) {
         </Grid.Row>
     );
 }
+
 export function ResourceDetailView({ resource }) {
     // Common props for grid row, columns that are re-usable.
     // If we need this in more than one place, consider re-making this into several components.
 
     return (
-        <div style={{ paddingTop: 30, paddingLeft: 100, paddingRight: 100 }}>
-            <Container>
-                <Menu text>
-                    <Menu.Item>
-                        <Header as="h2">
-                            <div>
-                                <span>
-                                    <Icon name="globe" />
-                                    <Header.Content id="title_header">{resource.title}</Header.Content>
-                                </span>
-                                <a href={resource.url} target="_blank" id="url">
-                                    <h4 className={linkStyles.link}>{resource.url}</h4>
-                                </a>
-                            </div>
-                            <Responsive {...Responsive.onlyMobile}>
-                                <Rating
-                                    icon="star"
-                                    rating={resource.rating}
-                                    maxRating={5}
-                                    disabled
-                                    size="massive"
-                                    style={{ paddingTop: 10 }}
-                                />
-                            </Responsive>
-                        </Header>
-                    </Menu.Item>
-                    <Menu.Item position="right">
-                        <Responsive minWidth={768}>
-                            <Rating icon="star" rating={resource.rating} maxRating={5} disabled size="massive" />
+        <Container>
+            <Menu text>
+                <Menu.Item>
+                    <Header as="h2">
+                        <div>
+                            <span>
+                                <Icon name="globe" />
+                                <Header.Content id="title_header">{resource.title}</Header.Content>
+                            </span>
+                            <a href={resource.url} target="_blank" id="url">
+                                <h4 className={linkStyles.link}>{resource.url}</h4>
+                            </a>
+                        </div>
+                        <Responsive {...Responsive.onlyMobile}>
+                            <Rating
+                                icon="star"
+                                rating={resource.rating}
+                                maxRating={5}
+                                disabled
+                                size="massive"
+                                style={{ paddingTop: 10 }}
+                            />
                         </Responsive>
-                    </Menu.Item>
-                </Menu>
+                    </Header>
+                </Menu.Item>
+                <Menu.Item position="right">
+                    <Responsive minWidth={768}>
+                        <Rating icon="star" rating={resource.rating} maxRating={5} disabled size="massive" />
+                    </Responsive>
+                </Menu.Item>
+            </Menu>
 
-                <Divider className={styles.dividerPadding} />
+            <Divider className={styles.dividerPadding} />
 
-                <Grid>
-                    {resource.created_by_user ? normal_grid_element("Submitted by:", resource.created_by_user) : null}
-                    {normal_grid_element("Date submitted:", resource.timestamp)}
-                    {normal_grid_element("Review status:", <p id="review_status"> {resource.review_status}</p>)}
-                    {normal_grid_element("Category:", <p id="category"> {resource.category} </p>)}
-                    {resource.tags && resource.tags.length > 0
-                        ? normal_grid_element(
-                              "Tags:",
-                              <div id="tags">
-                                  {resource.tags.map(tag => (
-                                      <Label key={tag} size="large">
-                                          {tag}
-                                      </Label>
-                                  ))}
-                              </div>
-                          )
-                        : null}
-                    {resource.attachment ? (
-                        <Grid.Row>
-                            <Grid.Column>
-                                <Header as="h5" color="grey">
-                                    <a
-                                        href={`/chatbotportal/resource/download-attachment/${resource.id}`}
-                                        // onClick={downloadAttachment}
-                                    >
-                                        <Icon name="download" />
-                                        <Header.Content>Download attachment</Header.Content>
-                                    </a>
-                                </Header>
-                            </Grid.Column>
-                        </Grid.Row>
-                    ) : null}
-                </Grid>
+            <Grid>
+                {resource.created_by_user ? normal_grid_element("Submitted by:", resource.created_by_user) : null}
+                {normal_grid_element("Date submitted:", resource.timestamp)}
+                {normal_grid_element("Review status:", <p id="review_status"> {resource.review_status}</p>)}
+                {normal_grid_element("Category:", <p id="category"> {resource.category} </p>)}
+                {resource.tags && resource.tags.length > 0
+                    ? normal_grid_element(
+                          "Tags:",
+                          <div id="tags">
+                              {resource.tags.map(tag => (
+                                  <Label key={tag} size="large">
+                                      {tag}
+                                  </Label>
+                              ))}
+                          </div>
+                      )
+                    : null}
 
-                <Divider />
+                {resource.attachment ? (
+                    <Grid.Row>
+                        <Grid.Column>
+                            <Header as="h5" color="grey">
+                                <a
+                                    href={`/chatbotportal/resource/download-attachment/${resource.id}`}
+                                    // onClick={downloadAttachment}
+                                >
+                                    <Icon name="download" />
+                                    <Header.Content>Download attachment</Header.Content>
+                                </a>
+                            </Header>
+                        </Grid.Column>
+                    </Grid.Row>
+                ) : null}
+            </Grid>
 
-                <Header as="h5" color="grey" className={styles.noMarginHeader}>
-                    <Icon name="comment" />
-                    <Header.Content>Comment:</Header.Content>
-                </Header>
-                <p id="comments" style={{ color: "grey" }}>
-                    {resource.comments}
-                </p>
-            </Container>
-        </div>
+            <Divider />
+
+            <Header as="h5" color="grey" className={styles.noMarginHeader}>
+                <Icon name="comment" />
+                <Header.Content>Comment:</Header.Content>
+            </Header>
+            <p id="comments" style={{ color: "grey" }}>
+                {resource.comments}
+            </p>
+        </Container>
     );
 }
