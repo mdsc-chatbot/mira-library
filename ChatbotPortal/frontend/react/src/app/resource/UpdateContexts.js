08743eef43bc72e3667e51eaf16ae500268874b6
apu031 <touhidul.apu@gmail.com>
2019-11-22 20:53:25 -0700

    UpdateContexts is added to update security context value token recieved through url and passed from ResourceSubmitFormExtension

---
+++ ChatbotPortal/frontend/react/src/app/resource/UpdateContexts.js
@@ -0,0 +1,21 @@
+import React, {Component, useCallback} from "react";
+import {SecurityContext} from "../contexts/SecurityContext";
+
+class UpdateContexts extends Component {
+    static contextType = SecurityContext;
+
+    componentDidMount() {
+        this.context.setSecurity({
+            is_logged_in: true,
+            token: this.props.token
+        });
+    }
+
+    render() {
+        return (
+            <div/>
+        );
+    }
+}
+
+export default UpdateContexts
