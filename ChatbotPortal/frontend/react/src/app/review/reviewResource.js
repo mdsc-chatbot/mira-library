2171f6978fc9f29b0386ece43285d13abed654d6
Jacy <crytechiee21@gmail.com>
2019-11-23 22:26:55 -0700

    updated review page resource view

--- ChatbotPortal/frontend/react/src/app/review/reviewResource.js
+++ ChatbotPortal/frontend/react/src/app/review/reviewResource.js
@@ -4,6 +4,8 @@ import {Container, Divider, Form, Header, Icon, Label, Rating} from "semantic-ui
 import {SecurityContext} from "../contexts/SecurityContext";
 import {baseRoute} from "../App";
 import {Link} from "react-router-dom";
+import ResourceResponsive from "../resource/ResourceResponsive";
+import { ResourceDetailView } from "../shared";
 
 export default class ResourceDetail extends Component {
     static contextType = SecurityContext;
@@ -169,65 +171,9 @@ export default class ResourceDetail extends Component {
                             {securityContext.security.is_logged_in ?
                                 <div>
                                     <Container>
-                                        <Header as="h3" style={{fontSize: "2em"}}>
-                                            <Icon name="globe"/>
-                                            <Header.Content>
-                                                {this.state.resource.title}
-                                            </Header.Content>
-                                        </Header>
-
-                                        <Divider></Divider>
-
-                                        <p style={{color: "grey", paddingTop: 25}}>
-                                            Submitted by {this.state.resource.created_by_user}
-                                        </p>
-                                        <p style={{color: "grey", marginTop: -10}}>
-                                            Date submitted: {this.state.resource.timestamp}
-                                        </p>
-
-                                        <a href={this.state.resource.url} target="_blank">
-                                            <h4>{this.state.resource.url}</h4>
-                                        </a>
-
-                                        {this.state.resource.rating ? (
-                                            <p>
-                                                <Rating
-                                                    icon="star"
-                                                    defaultRating={this.state.resource.rating}
-                                                    maxRating={5}
-                                                    disabled
-                                                    size="massive"
-                                                />
-                                            </p>
-                                        ) : null}
-                                        {this.state.resource.tags &&
-                                        this.state.resource.tags.length > 0 ? (
-                                            <p>
-                                                <span style={{color: "grey"}}>Tags:</span>
-                                                {this.state.resource.tags.map(tag => (
-                                                    <Label key={tag} size="large">
-                                                        {tag}
-                                                    </Label>
-                                                ))}
-                                            </p>
-                                        ) : null}
-                                        {this.state.resource.attachment ? (
-                                            <Header as="h5" color="grey">
-                                                <a href="#" onClick={this.downloadAttachment}>
-                                                    <Icon name="download"/>
-                                                    <Header.Content>
-                                                        Download attachment
-                                                    </Header.Content>
-                                                </a>
-                                            </Header>
-                                        ) : null}
-                                        <Header as="h5" color="grey">
-                                            <Icon name="comment"/>
-                                            <Header.Content>Comments:</Header.Content>
-                                        </Header>
-                                        <p style={{color: "grey", marginTop: -10}}>
-                                            {this.state.resource.comments}
-                                        </p>
+                                    <ResourceResponsive
+                                        resource_component={<ResourceDetailView resource={this.state.resource} />}
+                                    ></ResourceResponsive>
                                     </Container>
                                     <Container style={{width: "50%", height: "10%"}}>
                                         <h2>Submit Review</h2>
