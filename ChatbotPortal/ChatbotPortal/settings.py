3893320924b7da22291fb763d7644d695a92e245
apu031 <touhidul.apu@gmail.com>
2019-11-23 19:51:24 -0700

    Settings now have mySQL test database setup

--- ChatbotPortal/ChatbotPortal/settings.py
+++ ChatbotPortal/ChatbotPortal/settings.py
@@ -91,14 +91,17 @@ DATABASES = {
         'USER': 'root',
         'PASSWORD': '',
         'HOST': '',
-        'PORT': ''
+        'PORT': '',
+        'TEST': {
+            'NAME': 'test_main_db'
+        }
     }
 }
-if 'test' in sys.argv:
-    DATABASES['default'] = {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3'
-    }
+# if 'test' in sys.argv:
+#     DATABASES['default'] = {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': 'db.sqlite3'
+#     }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
