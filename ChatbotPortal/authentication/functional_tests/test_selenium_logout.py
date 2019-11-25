35be8900551b14b23307173e0fd266227e9ff582
apu031 <touhidul.apu@gmail.com>
2019-11-24 22:24:21 -0700

    Selenium test is optimized for logout

--- ChatbotPortal/authentication/functional_tests/test_selenium_logout.py
+++ ChatbotPortal/authentication/functional_tests/test_selenium_logout.py
@@ -1,5 +1,3 @@
-import time
-
 from django.test import LiveServerTestCase
 from selenium import webdriver
 
@@ -25,6 +23,7 @@ class TestLogout(LiveServerTestCase):
         self.active_user_password = '12345678'
         self.reset_db()
         self.browser = webdriver.Chrome()
+        self.browser.implicitly_wait(WAIT_SECONDS)
 
     def tearDown(self):
         """
@@ -57,6 +56,37 @@ class TestLogout(LiveServerTestCase):
         """
         CustomUser.objects.all().delete()
 
+    def logging_in(self, email=None, password=None):
+        """
+        This function opens the browser and finds the login related elements and perform certain actions on them.
+        :param email: test user's email
+        :type email: String
+        :param password: test user's password
+        :type password: String
+        :return: A reference to a the login_button situated in the html
+        :rtype: Address
+        """
+        self.browser.get('%s%s' % (self.live_server_url, LOGIN_PAGE))
+
+        # Finding the email field
+        email_element = self.browser.find_element_by_name('email')
+        self.assertIsNotNone(email_element)
+
+        # Finding the password field
+        password_element = self.browser.find_element_by_name('password')
+        self.assertIsNotNone(password_element)
+
+        if email is not None:
+            email_element.send_keys(email)
+        if password is not None:
+            password_element.send_keys(password)
+
+        # Finding the login button
+        login_button = self.browser.find_element_by_name('login_button')
+        self.assertIsNotNone(login_button)
+
+        return login_button
+
     def test_logout_menu_is_hidden_to_a_public_user(self):
         """
         Test public users should not be able to logout button
@@ -64,7 +94,6 @@ class TestLogout(LiveServerTestCase):
         """
 
         self.browser.get('%s%s' % (self.live_server_url, HOME_PAGE))
-        time.sleep(WAIT_SECONDS)
 
         # Finding the logout button
         logout_menu = self.browser.find_elements_by_link_text('Logout')
@@ -76,24 +105,12 @@ class TestLogout(LiveServerTestCase):
         :return: None
         """
 
-        self.browser.get('%s%s' % (self.live_server_url, HOME_PAGE))
-        time.sleep(WAIT_SECONDS)
+        login_button = self.logging_in(self.active_user_email, self.active_user_password)
+        login_button.click()
 
-        # Find the login menu
-        login_menu = self.browser.find_element_by_link_text('Login')
-        self.assertIsNotNone(login_menu)
-        login_menu.click()
-
-        time.sleep(WAIT_SECONDS)
-
-        # Logging in
-        self.browser.find_element_by_name('email').send_keys(self.active_user_email)
-        self.browser.find_element_by_name('password').send_keys(self.active_user_password)
-        self.browser.find_element_by_name('login_button').click()
-
-        time.sleep(WAIT_SECONDS)
-
-        # The user is redirected to the homepage
+        # A successful login redirects to the homepage
+        profile_option = self.browser.find_element_by_link_text('My Profile')
+        self.assertIsNotNone(profile_option)
         self.assertURLEqual(self.live_server_url + HOME_PAGE, self.browser.current_url)
 
         # Login should not be visible anymore menu
@@ -104,35 +121,19 @@ class TestLogout(LiveServerTestCase):
         logout_menu = self.browser.find_element_by_link_text('Logout')
         self.assertIsNotNone(logout_menu)
 
-        time.sleep(WAIT_SECONDS)
-
     def test_logout_successfully(self):
         """
         Test a logged in user can see the logout button
         :return: None
         """
 
-        self.browser.get('%s%s' % (self.live_server_url, HOME_PAGE))
-        time.sleep(WAIT_SECONDS)
+        login_button = self.logging_in(self.active_user_email, self.active_user_password)
+        login_button.click()
 
-        # Logging in
-        self.browser.find_element_by_link_text('Login').click()
-
-        time.sleep(WAIT_SECONDS)
-
-        # Logging in
-        self.browser.find_element_by_name('email').send_keys(self.active_user_email)
-        self.browser.find_element_by_name('password').send_keys(self.active_user_password)
-        self.browser.find_element_by_name('login_button').click()
-
-        time.sleep(WAIT_SECONDS)
-
-        # The user is redirected to the homepage
+        # A successful login redirects to the homepage
+        profile_option = self.browser.find_element_by_link_text('My Profile')
+        self.assertIsNotNone(profile_option)
         self.assertURLEqual(self.live_server_url + HOME_PAGE, self.browser.current_url)
-
-        # Login should not be visible anymore menu
-        login_menu = self.browser.find_elements_by_link_text('Login')
-        self.assertEqual(len(login_menu), 0)
 
         # Logout menu should be visible now
         logout_menu = self.browser.find_element_by_link_text('Logout')
@@ -141,17 +142,10 @@ class TestLogout(LiveServerTestCase):
         # Logging out
         logout_menu.click()
 
-        time.sleep(WAIT_SECONDS)
-
-        # The user is redirected to the homepage
-        self.assertURLEqual(self.live_server_url + HOME_PAGE, self.browser.current_url)
-
-        # Logout should be invisible again
-        logout_menu = self.browser.find_elements_by_link_text('Logout')
-        self.assertEqual(len(logout_menu), 0)
-
         # Login should be visible again
         login_menu = self.browser.find_elements_by_link_text('Login')
         self.assertEqual(len(login_menu), 1)
 
-        time.sleep(WAIT_SECONDS)
+        # Logout should be invisible again
+        logout_menu = self.browser.find_elements_by_link_text('Logout')
+        self.assertEqual(len(logout_menu), 0)
