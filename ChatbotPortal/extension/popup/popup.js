b68e51272836ee9056519d49c060fcb86e383598
apu031 <touhidul.apu@gmail.com>
2019-11-27 11:30:34 -0700

    License information and credits are added to the extension related files.

--- ChatbotPortal/extension/popup/popup.js
+++ ChatbotPortal/extension/popup/popup.js
@@ -1,3 +1,27 @@
+/**
+ * @file: popup.js
+ * @summary: Performs the operations as the popup gets mounted. It calls the logged in user and retrieve the JWT token.
+ *              Depending on its success, it renders a resource submission page or redirects to the login page in a new tab.
+ * @author: Apu Islam, Henry Lo, Jacy Mark, Ritvik Khanna, Yeva Nguyen
+ * @copyright: Copyright (c) 2019 BOLDDUC LABORATORY
+ * @credits: Apu Islam, Henry Lo, Jacy Mark, Ritvik Khanna, Yeva Nguyen
+ * @licence: MIT
+ * @version: 1.0
+ * @maintainer: BOLDDUC LABORATORY
+ */
+
+/**
+ * MIT License
+ *
+ * Copyright (c) 2019 BOLDDUC LABORATORY
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 const BASE_URL = 'http://127.0.0.1:8000';
 
 /**
