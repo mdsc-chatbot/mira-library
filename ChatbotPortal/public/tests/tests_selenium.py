c0153e96e1a6c1801b98532838ed9766a04ac750
tnguyen10 <tnguyen1@ualberta.ca>
2019-11-28 13:16:58 -0700

    test: public resources search, tags and categories filter

--- ChatbotPortal/public/tests/tests_selenium.py
+++ ChatbotPortal/public/tests/tests_selenium.py
@@ -43,7 +43,7 @@ from resource.models import Resource, Tag, Category
 class TestPublicPage(LiveServerTestCase):
     def setUp(self):
         self.driver = webdriver.Chrome()
-        self.driver.implicitly_wait(4)
+        self.driver.implicitly_wait(1)
         self.vars = {}
         self.setup_db()
         self.total_resources = 0
@@ -54,6 +54,16 @@ class TestPublicPage(LiveServerTestCase):
         Resource.objects.all().delete()
         Tag.objects.all().delete()
         Category.objects.all().delete()
+        CustomUser.objects.all().delete()
+
+        # Creating a user
+        self.user_email = "test@gmail.com"
+        self.user_password = "test"
+        user = CustomUser.objects.create_user(
+            email=self.user_email,
+            password=self.user_password,
+        )
+        user.save()
 
         # Creating tags
         english_tag = Tag.objects.create(name='English', approved=True)
@@ -71,9 +81,9 @@ class TestPublicPage(LiveServerTestCase):
         # 1. Resource with english tag, website
         # 2. Resource with french tag, pdf
         # 3. Resource with no tag or categories
-        resource1 = Resource.objects.create(title='MyHealth.Alberta.ca', url='https://myhealth.alberta.ca/', category=website_category, review_status='approved')
-        resource2 = Resource.objects.create(title='The Autism Research Institute', url='https://www.autism.org', website_summary_metadata='ARI works to advance the understanding of autism by funding research and facilitating education on its causes and the potential treatments.', category=pdf_category, review_status='approved')
-        resource3 = Resource.objects.create(title='Autism Edmonton', url='https://www.autismedmonton.org/', review_status='approved')
+        resource1 = Resource.objects.create(url='https://myhealth.alberta.ca/', category=website_category, review_status='approved')
+        resource2 = Resource.objects.create(url='https://www.autism.org', website_summary_metadata='ARI works to advance the understanding of autism by funding research and facilitating education on its causes and the potential treatments.', category=pdf_category, review_status='approved')
+        resource3 = Resource.objects.create(url='https://www.autismedmonton.org/', review_status='approved',category=website_category,)
 
         resource1.save()
         resource2.save()
@@ -90,15 +100,40 @@ class TestPublicPage(LiveServerTestCase):
 
     def test_public(self):
         self.driver.get('%s%s' % (self.live_server_url, "/chatbotportal/app"))
+        self.login()
+        self.resource_search()
+        self.resource_tag()
+        self.resource_category()
+    
+    def login(self):
         self.driver.find_element(By.LINK_TEXT, "Login").click()
         self.driver.find_element(By.NAME, "email").send_keys(self.user_email)
         self.driver.find_element(
             By.NAME, "password").send_keys(self.user_password)
         self.driver.find_element(By.NAME, "login_button").click()
 
-        self.resource_search()
-        self.resource_tag()
-        self.resource_category()
+
+    def check_search_results(self, should_find_results, should_not_find_results):
+        '''
+        Try to find search results elements
+        all test_found_results == should_find_results
+        all test_not_found_results == should_not_find_results  
+        '''
+        test_found_results = []
+        test_not_found_results = []
+        
+        for search_result in should_find_results + should_not_find_results:
+            try:
+                self.driver.find_element(By.LINK_TEXT, search_result)
+                test_found_results.append(search_result)
+            except:
+                test_not_found_results.append(search_result)
+
+        # print ("invalid", test_not_found_results, should_not_find_results)
+        # print ("valid", test_found_results, should_find_results)
+
+        assert test_found_results == should_find_results
+        assert test_not_found_results == should_not_find_results
 
     def go_to_public_resources(self):
         self.driver.find_element(By.LINK_TEXT, "My Resources").click()
@@ -106,45 +141,31 @@ class TestPublicPage(LiveServerTestCase):
 
     def resource_search(self):
         self.go_to_public_resources()
-
         self.driver.find_element(By.NAME, "searchBar").send_keys("MyHealth")
         self.driver.find_element(By.NAME, "searchButton").click()
-        self.driver.find_element(By.LINK_TEXT, "MyHealth.Alberta.ca").click()
 
-        self.go_to_public_resources()
-        self.driver.find_element(By.NAME, "searchBar").send_keys("The Autism Research Institute")
-        self.driver.find_element(By.NAME, "searchButton").click()
-        self.driver.find_element(By.LINK_TEXT, "The Autism Research Institute").click()
-
-        self.go_to_public_resources()
-        self.driver.find_element(By.NAME, "searchBar").send_keys("Autism Edmonton")
-        self.driver.find_element(By.NAME, "searchButton").click()
-        self.driver.find_element(By.LINK_TEXT, "Autism Edmonton").click()
-
+        self.check_search_results(["MyHealth.Alberta.ca"], 
+        ["The Autism Research Institute | #1 Advocate for Autism Research | Home", "Autism Edmonton"])
 
     def resource_tag(self):
         self.go_to_public_resources()
-
-        self.driver.find_element(By.NAME, "English").click()
-        self.driver.find_element(By.LINK_TEXT, "MyHealth.Alberta.ca").click()
-
-        self.go_to_public_resources()
-        self.driver.find_element(By.NAME, "French").click()
-        self.driver.find_element(By.LINK_TEXT, "The Autism Research Institute").click()
+        self.driver.find_element(By.XPATH, "//label[contains(.,'English')]").click()
+        self.check_search_results(["MyHealth.Alberta.ca"], 
+        ["The Autism Research Institute | #1 Advocate for Autism Research | Home", "Autism Edmonton"])
 
         self.go_to_public_resources()
-        self.driver.find_element(By.LINK_TEXT, "Autism Edmonton").click()
+        self.driver.find_element(By.XPATH, "//label[contains(.,'French')]").click()
+        self.check_search_results(["The Autism Research Institute | #1 Advocate for Autism Research | Home"], 
+        ["MyHealth.Alberta.ca", "Autism Edmonton"])
 
     def resource_category(self):
         self.go_to_public_resources()
-
-        self.driver.find_element(By.NAME, "Website").click()
-        self.driver.find_element(By.LINK_TEXT, "MyHealth.Alberta.ca").click()
-
-        self.go_to_public_resources()
-        self.driver.find_element(By.NAME, "PDF").click()
-        self.driver.find_element(By.LINK_TEXT, "The Autism Research Institute").click()
+        self.driver.find_element(By.XPATH, "//label[contains(.,'Website')]").click()
+        self.check_search_results(["MyHealth.Alberta.ca", "Autism Edmonton"], 
+        ["The Autism Research Institute | #1 Advocate for Autism Research | Home"])
 
         self.go_to_public_resources()
-        self.driver.find_element(By.LINK_TEXT, "Autism Edmonton").click()
+        self.driver.find_element(By.XPATH, "//label[contains(.,'PDF')]").click()
+        self.check_search_results(["The Autism Research Institute | #1 Advocate for Autism Research | Home"], 
+        ["MyHealth.Alberta.ca", "Autism Edmonton"])
 
