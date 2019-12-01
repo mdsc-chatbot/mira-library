a0657f83e1b1dab2359b121f020b687926a17a9d
apu031 <touhidul.apu@gmail.com>
2019-11-30 20:14:43 -0700

    Removed commented out cors origin

--- ChatbotPortal/chatbotPortal/settings.py
+++ ChatbotPortal/chatbotPortal/settings.py
@@ -197,10 +197,6 @@ JWT_AUTH = {
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
-# CORS_ORIGIN_WHITELIST = [
-#     'http://localhost:3000',
-#     'http://localhost:8000',
-# ]
 
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:8000',
