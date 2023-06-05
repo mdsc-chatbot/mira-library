f35271e975cd0ed9cd42c7c92ce7335137a4cee9
alexlf <alexlf@live.ca>
2023-06-05 16:45:58 -0400

    Remove refrences to removed functions

--- ChatbotPortal/public/urls.py
+++ ChatbotPortal/public/urls.py
@@ -22,7 +22,7 @@ __maintainer__ = "BOLDDUC LABORATORY"
 #  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 from django.urls import path
-from .views import ResourceView, ResourceByIntentEntityView, get_tag_group_stats, TagView, AllTagView, CategoryView, DetailedResourceView, DetailedResourceAdminView, HomepageResourceView, IndexResourceEntityView, VerifyApprovedResourcesView, ResourceCountAndFilterView, ResourceStatsView, ResourceByIntentEntityView_new, AddViewOfResourceView, ResourceByIntentEntityView_new_new, EmotionTest#, add_tag_relation, get_relations_by_tag
+from .views import ResourceView, get_tag_group_stats, TagView, CategoryView, DetailedResourceView, DetailedResourceAdminView, HomepageResourceView, IndexResourceEntityView, VerifyApprovedResourcesView, ResourceCountAndFilterView, ResourceStatsView, ResourceByIntentEntityView_new, AddViewOfResourceView, ResourceByIntentEntityView_new_new, EmotionTest#, add_tag_relation, get_relations_by_tag
 
 urlpatterns = [
     # path('current_user/', current_user, name='current_user'),
